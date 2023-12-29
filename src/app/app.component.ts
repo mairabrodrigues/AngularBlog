4a77e76dfebc12650346664df4f475551baf4162
maira <Mairabrodrigues@gmail.com>
2023-12-29 19:02:46 +0000

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MenuBarComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule,MenuTitleComponent,BigCardComponent,SmallCardComponent,MenuBarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
